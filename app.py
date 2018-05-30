1926c00e9317973e6b4a16e61fec45538eab9f74
ibalihikya <ibalihikya@users.noreply.github.com>
2018-05-30 15:25:00 +0300

    add more tests

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
 from flask import Flask, jsonify, request, Response, json
-app= Flask(__name__)
+
+app = Flask(__name__)
 
 
-user_requests = [{'userid':1,
+user_requests = [{'userid': 1,
                   'id': 1,
-                   'title': 'leaking pipe',
-                    'description': 'small leakage reducing engine pressure'},
-                {'userid':1,
-                    'id': 2,
-                   'title': 'broken handle',
-                    'description': 'The handle of the front door is broken'},
-                    {'userid':1,
-                        'id': 3,
-                   'title': 'flat tyre',
-                    'description': 'My car tyre is flat'}]
- 
+                  'title': 'leaking pipe',
+                  'description': 'small leakage reducing engine pressure',
+                  'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+                  'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+                  'status': 'pending'},
+                 {'userid': 1,
+                  'id': 2,
+                  'title': 'broken handle',
+                  'description': 'The handle of the front door is broken',
+                  'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+                  'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+                  'status': 'pending'},
+                 {'userid': 1,
+                  'id': 3,
+                  'title': 'flat tyre',
+                  'description': 'My car tyre is flat',
+                  'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+                  'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+                  'status': 'pending'}]
+                  
 
-@app.route('/api/v1/users/requests', methods = ['GET'])
+
+@app.route('/api/v1/users/requests', methods=['GET'])
 def get_all_user_requests():
-    resp = jsonify({'user_requests' : user_requests})
-    resp.status_code = 200
+    resp = jsonify({'user_requests': user_requests})
+    #resp.status_code = 200
     return resp
 
+@app.route('/api/v1/users/requests/<int:requestId>', methods=['GET'])
+def get_specific_user_request(requestId):
+    return jsonify({'':''})
+
+@app.route('/api/v1/users/requests', methods=['POST'])
+def create_user_request():
+    return jsonify({'':''})
+    
 
 if __name__ == '__main__':
-    app.run()
+         app.run()
--- test_endpoints.py
+++ test_endpoints.py
@@ -1,27 +1,65 @@
-from app import*
+from app import app
 import unittest
+import json
 
-expected_response = {
-  "user_requests": [
-    {
-      "userid": 1,
-      "description": "small leakage reducing engine pressure",
-      "id": 1,
-      "title": "leaking pipe"
-    },
-    {
-      "userid": 1,
-      "description": "The handle of the front door is broken",
-      "id": 2,
-      "title": "broken handle"
-    },
-    {
-      "userid" : 1,
-      "description": "My car tyre is flat",
-      "id": 3,
-      "title": "flat tyre"
-    }
-  ]
+
+
+u_requests ={
+        "user_requests":[
+		          {'userid': 1,
+                  'id': 1,
+                  'title': 'leaking pipe',
+                  'description': 'small leakage reducing engine pressure',
+                  'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+                  'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+                  'status': 'pending'},
+                 {'userid': 1,
+                  'id': 2,
+                  'title': 'broken handle',
+                  'description': 'The handle of the front door is broken',
+                  'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+                  'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+                  'status': 'pending'},
+                 {'userid': 1,
+                  'id': 3,
+                  'title': 'flat tyre',
+                  'description': 'My car tyre is flat',
+                  'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+                  'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+                  'status': 'pending'}
+				  ]
+				  }
+
+# this should not be a json array
+u_request = {
+    "user_request":
+		          {'userid': 1,
+                              'id': 1,
+                              'title': 'leaking pipe',
+                              'description': 'small leakage reducing engine pressure',
+                              'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+                              'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+                              'status': 'pending'}
+}
+
+u_entry = {
+    'userid': 1,
+    'title': 'leaking pipe',
+    'description': 'small leakage reducing engine pressure',
+    'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+    'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+    'status': 'pending'
+}
+
+u_request2 = {
+    "user_request":
+		          {'userid': 1,
+                              'id': 4,
+                              'title': 'leaking pipe',
+                              'description': 'small leakage reducing engine pressure',
+                              'date_created': 'Wed, 30 May 2018 14:00:00 GMT',
+                              'date_modified': 'Wed, 30 May 2018 15:00:00 GMT',
+                              'status': 'pending'}
 }
 
 
@@ -30,7 +68,27 @@ class RestTests(unittest.TestCase):
         testobj = app.test_client(self)
         response = testobj.get('/api/v1/users/requests', content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), expected_response)
+        self.assertEqual(response.get_json(), u_requests)
+
+    def test_get_specific_user_request(self):
+            testobj = app.test_client(self)
+            response = testobj.get('/api/v1/users/requests/1', content_type='application/json')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.get_json(), u_request)
+
+    def test_get_non_existent_user_request(self):
+            testobj = app.test_client(self)
+            response = testobj.get('/api/v1/users/requests/15', content_type='application/json')
+            self.assertEqual(response.status_code, 404)
+            #self.assertEqual(response.get_json(), expected_response)
+
+    def test_create_request(self):
+        testobj = app.test_client(self)
+        response = testobj.post('/api/v1/users/requests', json=u_entry)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.get_json(), u_request2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
