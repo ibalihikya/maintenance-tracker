ce27396b2df287aaf1ddd1a9b80ca7a89ea93266
ibalihikya <ibalihikya@users.noreply.github.com>
2018-06-04 07:28:36 +0300

    Add .gitattributes file

--- api/views.py
+++ api/views.py
@@ -7,7 +7,6 @@ user_requests = []
 @app.route('/api/v1/users/requests', methods=['GET'])
 def get_all_user_requests():
     response = jsonify({'user_requests':user_requests})
-    response.status_code = 200
     return response
 
 @app.route('/api/v1/users/requests/<int:requestId>', methods=['GET'])
@@ -22,16 +21,12 @@ def get_specific_user_request(requestId):
         response.status_code = 404
     else: 
         response = jsonify({'user_request': ur})
-        response.status_code = 200
     
     return response
 
 @app.route('/api/v1/users/requests', methods=['POST'])
 def create_user_request():
-    #parse json response to a dictionary
     ur =  request.get_json(force=True)
-
-    #assign id to the user request
     input_dictionary = {
         'userid' : ur['userid'],
         'id' : len(user_requests) + 1,
@@ -40,18 +35,15 @@ def create_user_request():
         'date_created' : ur['date_created'],
         'date_modified' : ur['date_modified'],
         'status' : ur['status']}
-    
-    #add dictionary to the list of requests
+
     user_requests.append(input_dictionary)
     
-    #assign id to the user request
     response = jsonify(input_dictionary)
     response.status_code = 201
     return response
 
 @app.route('/api/v1/users/requests/<int:requestId>', methods=['PUT'])
 def modify_request(requestId):
-    #Response.status_code = 200
     data_posted =  request.get_json(force=True)
     ur = {}
     for u_request in user_requests:
@@ -59,9 +51,7 @@ def modify_request(requestId):
             ur = u_request
             break
     ur['title'] = data_posted['title'] 
-    #response = jsonify({'':''})
     response = jsonify(ur)
-    response.status_code = 200
     return response
 
 @app.route('/api/v1/users/requests', methods=['DELETE'])
